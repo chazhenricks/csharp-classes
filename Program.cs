b6efe8836c671a22e38dba552fdbf3bd83b53872
Chaz Henricks <chazhenricks@gmail.com>
2017-07-10 15:02:22 -0500

    WORKS

--- Program.cs
+++ Program.cs
@@ -3,42 +3,76 @@ using System.Collections.Generic;
 
 namespace classes
 {
-  public class Company
+    public class Company
     {
         /*
             Some readonly properties
         */
-            //these are private fields that hold the name, jobTitle and startDate of an employee. 
-            private string name;
-            private string jobTitle;
-            private string startDate;
-  
         public string Name { get; }
         public DateTime CreatedOn { get; }
 
         // Create a property for holding a list of current employees
-        public List<string> employeeList = new List<string>();
+
+        public static List<Employee> EmployeeList = new List<Employee>();
 
         // Create a method that allows external code to add an employee
-
-        public void addEmplyee(string employee){
-            employeeList.Add(employee);
+        public static void AddEmployee(Employee emp){
+            EmployeeList.Add(emp);
         }
-
         // Create a method that allows external code to remove an employee
-
-        public void removeEmployee(string employee){
-            employeeList.Remove(employee);
+        public static void RemoveEmployee(Employee emp){
+            EmployeeList.Remove(emp);
         }
         /*
             Create a constructor method that accepts two arguments:
                 1. The name of the company
                 2. The date it was created
-
             The constructor will set the value of the public properties
         */
-            public void Employee(string employeeName){
-            name = employeeName;
+
+        public Company(string name){
+            this.Name = name;
+            this.CreatedOn = new DateTime();
+        }
+    }
+    
+    public class Employee{
+
+        public string name;
+        public string jobTitle;
+        public string startDate;
+
+         /*
+         Methods
+          */
+          public Employee(string Name, string Job, string Date){
+              this.name = Name;
+              this.jobTitle = Job;
+              this.startDate = Date;
+          }
+    }
+
+    public class Program{
+
+        public static void Main(){
+
+            new Company("NSS");
+
+            Company.EmployeeList.Add(new Employee("Chaz", "Developer", "Sept 22"));
+            Company.EmployeeList.Add(new Employee("Matt", "Cool Guy", "Sept 21"));
+            Company.EmployeeList.Add(new Employee("Steve", "Does Stuff", "Sept 20"));
+
+            // List<Employee> EmployeeList = new List<Employee>(){
+            // new Employee("Chaz", "Developer", "Sept 22"),
+            // new Employee("Tiny Rick", "Scientist", "Sept 21"),
+            // new Employee("Steve", "Janitor", "Sept 23")
+        // };
+
+
+            foreach(Employee e in Company.EmployeeList){
+                Console.WriteLine($"{e.name} {e.jobTitle} {e.startDate}");
+            }
+
         }
     }
 }
